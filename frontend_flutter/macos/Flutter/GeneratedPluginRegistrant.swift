81e7c8e5646f516c073a7404c37ff076f6ed43d0
Sumukh Paspuleti <156015540+SumukhP-dev@users.noreply.github.com>
2025-12-22 08:40:56 -0500

    chore: commit latest Dream Flow features (no builds/models)

--- frontend_flutter/macos/Flutter/GeneratedPluginRegistrant.swift
+++ frontend_flutter/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,30 +5,20 @@
 import FlutterMacOS
 import Foundation
 
-import app_links
 import audio_session
 import just_audio
-import package_info_plus
 import path_provider_foundation
 import purchases_flutter
-import sentry_flutter
-import share_plus
 import shared_preferences_foundation
 import url_launcher_macos
 import video_player_avfoundation
-import wakelock_plus
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  AppLinksMacosPlugin.register(with: registry.registrar(forPlugin: "AppLinksMacosPlugin"))
   AudioSessionPlugin.register(with: registry.registrar(forPlugin: "AudioSessionPlugin"))
   JustAudioPlugin.register(with: registry.registrar(forPlugin: "JustAudioPlugin"))
-  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   PurchasesFlutterPlugin.register(with: registry.registrar(forPlugin: "PurchasesFlutterPlugin"))
-  SentryFlutterPlugin.register(with: registry.registrar(forPlugin: "SentryFlutterPlugin"))
-  SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
   FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
-  WakelockPlusMacosPlugin.register(with: registry.registrar(forPlugin: "WakelockPlusMacosPlugin"))
 }
